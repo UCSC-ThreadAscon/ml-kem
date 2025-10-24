5cca0e55f2cd670b7552edfbaf0700c0c7052cd3
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2025-10-24 12:30:18 -0700

    Adding ML-KEM header and getting build errors

--- main/src/main.c
+++ main/src/main.c
@@ -2,6 +2,10 @@
 #include "tight_loop.h"
 #include "independent_variables.h"
 
+#define MLK_CONFIG_API_PARAMETER_SET MLK_CONFIG_PARAMETER_SET
+#define MLK_CONFIG_API_NAMESPACE_PREFIX mlkem
+#include "mlkem/mlkem_native.h"
+
 #if ((DELAY_SERVER || DELAY_CLIENT) && !CONFIG_OPENTHREAD_TIME_SYNC)
 #error "You must TURN ON Time Sync for the Delay experiments."
 #elif (!(DELAY_SERVER || DELAY_CLIENT) && CONFIG_OPENTHREAD_TIME_SYNC)
