2ee6bd577de070b258ce126ef3d4766b73fced2d
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2025-10-24 15:45:09 -0700

    Removing ML-KEM

--- main/src/main.c
+++ main/src/main.c
@@ -2,25 +2,6 @@
 #include "tight_loop.h"
 #include "independent_variables.h"
 
- // NOTE: `mlkem` must NOT be written in quotes.
-#define MLK_CONFIG_API_NAMESPACE_PREFIX crypto_kem
-#define MLK_CONFIG_API_PARAMETER_SET 512
-#include "mlkem/mlkem_native.h"
-
-#include "notrandombytes.h"
-
-#define CHECK(x)                                              \
-  do                                                          \
-  {                                                           \
-    int rc;                                                   \
-    rc = (x);                                                 \
-    if (!rc)                                                  \
-    {                                                         \
-      fprintf(stderr, "ERROR (%s,%d)\n", __FILE__, __LINE__); \
-      return;                                                 \
-    }                                                         \
-  } while (0)
-
 #if ((DELAY_SERVER || DELAY_CLIENT) && !CONFIG_OPENTHREAD_TIME_SYNC)
 #error "You must TURN ON Time Sync for the Delay experiments."
 #elif (!(DELAY_SERVER || DELAY_CLIENT) && CONFIG_OPENTHREAD_TIME_SYNC)
@@ -30,65 +11,5 @@
 void app_main(void)
 {
   startMain();
-
-  uint8_t pk[CRYPTO_PUBLICKEYBYTES];
-  uint8_t sk[CRYPTO_SECRETKEYBYTES];
-  uint8_t ct[CRYPTO_CIPHERTEXTBYTES];
-  uint8_t key_a[CRYPTO_BYTES];
-  uint8_t key_b[CRYPTO_BYTES];
-
-  /* The PCT modifies the PRNG state, so the KAT tests don't work.
-   * We run KAT tests only for disabled PCT. */
-  const uint8_t expected_key[] = {
-      0x77, 0x6c, 0x74, 0xdf, 0x30, 0x1f, 0x8d, 0x82, 0x52, 0x5e, 0x8e,
-      0xbb, 0xb4, 0x00, 0x95, 0xcd, 0x2e, 0x92, 0xdf, 0x6d, 0xc9, 0x33,
-      0xe7, 0x86, 0x62, 0x59, 0xf5, 0x31, 0xc7, 0x35, 0x0a, 0xd5};
-
-  /* WARNING: Test-only
-   * Normally, you would want to seed a PRNG with trustworthy entropy here. */
-  randombytes_reset();
-
-  printf("Generating keypair ... ");
-
-  /* Alice generates a public key */
-  CHECK(crypto_kem_keypair(pk, sk) == 0);
-
-  printf("DONE\n");
-  printf("Encaps... ");
-
-  /* Bob derives a secret key and creates a response */
-  CHECK(crypto_kem_enc(ct, key_b, pk) == 0);
-
-  printf("DONE\n");
-  printf("Decaps... ");
-
-  /* Alice uses Bobs response to get her shared key */
-  CHECK(crypto_kem_dec(key_a, ct, sk) == 0);
-
-  printf("DONE\n");
-  printf("Compare... ");
-
-  CHECK(memcmp(key_a, key_b, CRYPTO_BYTES) == 0);
-
-  printf("Shared secret: ");
-  {
-    size_t i;
-    for (i = 0; i < sizeof(key_a); i++)
-    {
-      printf("%02x", key_a[i]);
-    }
-  }
-  printf("\n");
-
-#if !defined(MLK_CONFIG_KEYGEN_PCT)
-  /* Check against hardcoded result to make sure that
-   * we integrated custom FIPS202 correctly */
-  CHECK(memcmp(key_a, expected_key, CRYPTO_BYTES) == 0);
-#else
-  printf(
-      "[WARNING] Skipping KAT test since PCT is enabled and modifies PRNG\n");
-#endif
-
-  printf("OK\n");
   return;
 }
