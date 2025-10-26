74acb7daa03a7e63a3d047abdab19e63d2d33d70
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2025-10-26 13:06:46 -0700

    The imports work

--- main/src/main.c
+++ main/src/main.c
@@ -2,7 +2,23 @@
 #include "tight_loop.h"
 #include "independent_variables.h"
 
-#include "kem.h"
+#define MLK_CONFIG_API_PARAMETER_SET 512
+#define MLK_CONFIG_API_NAMESPACE_PREFIX mlkem
+
+#include "mlkem_native.h"
+#include "notrandombytes.h"
+
+#define CHECK(x)                                              \
+  do                                                          \
+  {                                                           \
+    int rc;                                                   \
+    rc = (x);                                                 \
+    if (!rc)                                                  \
+    {                                                         \
+      fprintf(stderr, "ERROR (%s,%d)\n", __FILE__, __LINE__); \
+      return 1;                                               \
+    }                                                         \
+  } while (0)
 
 void app_main(void)
 {
